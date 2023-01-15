deea51309a3251f14dd4ae140b2bcc47c10da55a
Ludwig Zellner <zellner-ludwig@t-online.de>
2023-01-15 18:40:03 +0100

    Started feature breath length

--- public/js/meditation-support.js
+++ public/js/meditation-support.js
@@ -1,7 +1,5 @@
-const SHOWN_TIME = 5000;
-const HIDDEN_TIME = 1500;
-const FADING_OUT_TIME = 2000;
-const FADING_IN_TIME = 1500;
+var delay_time = 2000;
+const FADING_TIME = 1500;
 
 var changetext_timeout = null;
 var text_timeout = null;
@@ -30,18 +28,33 @@ function changeText(out) {
     
 }
 
+function resetMeditationSupport() {
+    $("#meditation-support").stop(true, true).promise().done(function () {
+        meditationSupportContainer.style.display = "none";
+        changeText(true);
+    });
+}
+
+$(function () {
+    $("#meditation-time").on('change', function () {
+        delay_time = $("#meditation-time").val() * 1000;
+        console.log("Setting delay time to", delay_time);
+        resetMeditationSupport();
+        meditationSupport(false);
+    })
+});
+
 function meditationSupport(out) {
     if (input.checked) {
-        $("#meditation-support").fadeIn(1500).delay(2000).fadeOut(1500).delay(2000).promise().done(function () {
+        $(".meditation-support-time").css("display", "block");
+        $("#meditation-support").fadeIn(FADING_TIME).delay(delay_time).fadeOut(FADING_TIME).delay(delay_time).promise().done(function () {
             changeText(out);
             out = out ? false : true;
             meditationSupport(out);
         });
     } else {
-        $("#meditation-support").stop(true, true).promise().done(function () {
-            meditationSupportContainer.style.display = "none";
-            changeText(true);
-        });
+        $(".meditation-support-time").css("display", "none");
+        resetMeditationSupport();
         
     }
 }
